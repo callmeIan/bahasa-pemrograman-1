1aceb1ea694ee6f9e5952de6e7a6f811e63d32f0
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:22:12 +0700

    Latihan data 1a

--- src/main/kotlin/Latihan2.kt
+++ src/main/kotlin/Latihan2.kt
@@ -1,5 +1,18 @@
-fun main(){
-    println ("Nama    : Destyan Yoga A")
-    println ("Alamat  : Yogyakarta")
-    println ("Jurusan : Sistem Imformasi")
+fun main() {
+  //pertemuan 1
+  //println("Nama: Destyan Yoga A")
+  //println("Alamat  : Yogyakarta")
+  //println("Jurusan : Sistem Imformasi")
 }
+  //pertemuan 2
+  fun main (args: Array<String>){
+  var namaLengkap: String = "Destyan Yoga A"
+  var alamat: String
+  alamat = "Yogyakarta"
+  var umur = "20"
+
+  println(namaLengkap)
+  println(alamat)
+  println(umur)
+}
+
